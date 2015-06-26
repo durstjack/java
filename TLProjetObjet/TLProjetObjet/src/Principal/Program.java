5176de3b481a441bb91c7d9aa8ae827ff8f3377a
Thierry Lalandre <durstjack@gmail.com>
2015-06-26 11:18:46 +0200

    2 types de constructeurs

--- TLProjetObjet/TLProjetObjet/src/Principal/Program.java
+++ TLProjetObjet/TLProjetObjet/src/Principal/Program.java
@@ -1,10 +1,26 @@
 package Principal;
+import java.io.ObjectOutput;
+import java.util.Arrays;
+
+import metier.Livre;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-
+		//metier.Livre l1;
+		Livre L1;
+		L1 = new Livre();
+		L1.titre = "La belle au bois dormant";
+		L1.isbn = "3655236325";
+		L1.prix = 22.63;
+		
+		Livre L2 = new Livre();
+		L2.titre = "Le chateau de monpère";
+		L2.isbn = "1423652362";
+		L2.prix = 32.90;
+		
+		L1.afficher();
+		
 	}
 
 }
--- TLProjetObjet/TLProjetObjet/src/metier/Livre.java
+++ TLProjetObjet/TLProjetObjet/src/metier/Livre.java
@@ -1,5 +1,31 @@
 package metier;
 
 public class Livre {
+	public String titre;
+	public String isbn;
+	public double prix;
+	
+	//java fournit un constructeur par défault, sauf à partir du moment ou on en definit un
+	//il ne definit plus de constructeur par défault
+	//le constructeur n a pas de type de retour, doit avoir le meme nom que la classe
+	public Livre(){
+		System.out.println("Construction du livre........");
+		titre = "inconnu";
+		isbn = "--------";
+		prix = 0;
+	}
+	//on peut avoir plusieurs commentaires, comme pour les methodes si la signature n'est pas la même
+	// on rajoute this pour lever l'anbiguité avec la variable locale et l'attribut de l'objet
+	public Livre(String titre, String isbn, double prix){
+		System.out.println("Construction du livre........");
+		this.titre = titre;
+		this.isbn = isbn;
+		this.prix = prix;
+	}
+	
+	public void afficher(){
+		System.out.println(this.titre + ", "+ prix +" euros");
+	}
+	
 
 }
