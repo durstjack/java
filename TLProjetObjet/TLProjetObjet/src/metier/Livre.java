7216b8e3dda35ec9264e62eee139ef08b90d5f14
Thierry Lalandre <durstjack@gmail.com>
2015-06-26 11:23:47 +0200

    on utilise this() pour le chainage de constructeur

--- TLProjetObjet/TLProjetObjet/src/metier/Livre.java
+++ TLProjetObjet/TLProjetObjet/src/metier/Livre.java
@@ -9,10 +9,15 @@ public class Livre {
 	//il ne definit plus de constructeur par défault
 	//le constructeur n a pas de type de retour, doit avoir le meme nom que la classe
 	public Livre(){
+	//si on fait un chainage de constructeur  on doit commencer par ca pas de code avant genre syso etc...
+	// on utilise this()
+		this("inconnu", "00000", 0.01);		
+	/*	
 		System.out.println("Construction du livre........");
 		titre = "inconnu";
 		isbn = "--------";
 		prix = 0;
+	*/
 	}
 	//on peut avoir plusieurs commentaires, comme pour les methodes si la signature n'est pas la même
 	// on rajoute this pour lever l'anbiguité avec la variable locale et l'attribut de l'objet
