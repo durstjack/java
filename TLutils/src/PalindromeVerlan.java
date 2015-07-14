fe146a307eb129e67998ecafa09ca8a580ae530b
Thierry Lalandre <durstjack@gmail.com>
2015-07-14 17:19:57 +0200

    fonction pour tester si un mot est un palindrome

---
+++ TLutils/src/PalindromeVerlan.java
@@ -0,0 +1,27 @@
+
+public class PalindromeVerlan {
+
+	public static void main(String[] args) {
+
+		System.out.println("'serres' est un palindrome ? " + estPalindrome("serres"));
+		System.out.println("'bonjour' est un palindrome ? " + estPalindrome("bonjour"));
+		System.out.println("'abcdcba' est un palindrome ? " + estPalindrome("abcdcba"));
+		System.out.println("'abedcba' est un palindrome ? " + estPalindrome("abedcba"));
+
+	}
+	
+	public static boolean estPalindrome(String chaine) {
+		int start = 0;
+		int end = chaine.length() - 1;
+		//on a "deux pointers", un au debut et un a la fin
+		//on les ccompare, puis on avance d un cran etc etc
+		while (start < end) {
+			if (chaine.charAt(start) != chaine.charAt(end))
+				return false;
+			start++;
+			end--;
+		}
+		return true;
+	}
+
+}
