0d21513a14ec8f90678b1fe6cf2c46732b5f4f02
Thierry Lalandre <durstjack@gmail.com>
2015-07-14 23:02:48 +0200

    ajout de ObjetsDeBase

---
+++ TLutils/src/ObjetsDeBase/metier/Livre.java
@@ -0,0 +1,53 @@
+package ObjetsDeBase.metier;
+
+public class Livre {
+	
+	private static int compteur = 0;
+	public static int getCompteur() {
+		return compteur;
+	}	
+	
+	private String titre;
+	private String isbn;
+	private double prix;
+	
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = (prix > 0.0)? prix : 0.0;
+	}
+	
+	
+	public Livre() {
+		//appel le constructeur ci dessus avec 3 attributs fixés d'avance
+		//en cas d'utilisation du constructeur simple Livre()
+		this("inconnu", "0000000000", 0.01);
+	}
+	
+	public Livre(String titre, String isbn, double prix) {
+		System.out.println("construction livre 2 ...");
+		setTitre(titre);
+		setIsbn(isbn);
+		setPrix(prix);
+		compteur++;
+	}
+	
+	
+	public void afficher() {
+		System.out.println(this.titre + ", " + prix + " euros");
+	}
+
+}
---
+++ TLutils/src/ObjetsDeBase/principal/Program.java
@@ -0,0 +1,29 @@
+package ObjetsDeBase.principal;
+
+//import metier.Livre;
+import ObjetsDeBase.metier.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Livre.getCompteur());
+		
+		//metier.Livre l1;
+		Livre l1;
+		l1 = new Livre("20000 lieux sous les mers", "3435434354", 22.99);		
+		l1.afficher();
+		
+		
+		Livre l2 = new Livre();
+		
+		System.out.println(l1.getPrix());
+		l1.setPrix(-45.5);
+		
+		l1.afficher();
+		
+		System.out.println(Livre.getCompteur());
+		
+	}
+
+}
