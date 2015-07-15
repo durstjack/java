328bcd034c91db0c78375ade4eea4147e65c1dca
Thierry Lalandre <durstjack@gmail.com>
2015-07-15 12:18:54 +0200

    chargement du fichier driver

--- WebFirstJDBC/src/com/loconto/webfirstjdbc/utils/DatabaseManager.java
+++ WebFirstJDBC/src/com/loconto/webfirstjdbc/utils/DatabaseManager.java
@@ -1,5 +1,9 @@
 package com.loconto.webfirstjdbc.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -9,20 +13,46 @@ import javax.servlet.ServletContextListener;
  */
 public class DatabaseManager implements ServletContextListener {
 
-    // Default constructor. 
+    private Connection base;
+	
+	//constructor. 
     public DatabaseManager() {
-        // TODO Auto-generated constructor stub
+
     }
 
 	// Initialisation de la WebApp - @see ServletContextListener#contextInitialized(ServletContextEvent)
-    public void contextInitialized(ServletContextEvent arg0)  { 
+    public void contextInitialized(ServletContextEvent ce)  { 
          System.out.println("demarrage de DatabaseManager");
+         String driverclass = ce.getServletContext().getInitParameter("driverclass");
+         String databaseurl = ce.getServletContext().getInitParameter("databaseurl");
+         String login = ce.getServletContext().getInitParameter("login");
+         String password = ce.getServletContext().getInitParameter("password");
          
+         //chargement du driver mysql
+         try 
+         {        	 
+			Class.forName(driverclass); // charge la classe dont on donne le nom en parametre
+			base = DriverManager.getConnection(databaseurl, login, password);
+			System.out.println("connecté!!!!");
+						
+		} 
+         catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+         catch (SQLException e) {
+			e.printStackTrace();
+		}
+         //! fin du chargement du driver mysql
 
+         
     }
 	// Fin de la WebApp - @see ServletContextListener#contextDestroyed(ServletContextEvent)
      public void contextDestroyed(ServletContextEvent arg0)  { 
-
+    	 try {
+			base.close();//fermeture de la connection a la base de données
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
 
