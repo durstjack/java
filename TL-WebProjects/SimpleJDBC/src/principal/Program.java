480da9982e8de6515c13bd1aa4db2b3cb65f04a0
Thierry Lalandre <durstjack@gmail.com>
2015-07-26 18:01:44 +0200

    ajout de texte dans le println

--- TL-WebProjects/SimpleJDBC/src/principal/Program.java
+++ TL-WebProjects/SimpleJDBC/src/principal/Program.java
@@ -8,30 +8,24 @@ public class Program {
 		try {
 			// chargement/initialisation du driver MYSQL
 			Class.forName("com.mysql.jdbc.Driver");
-			// Connection est l'interface décrivant un objet permettant de se connecter
-			// a une base de donnée
-			// le DriverManager renvoie un objet implémentant cette interface
-			// qui est capable de gérer l'url qu'on lui a fournie
-		    Connection base =
-		    		DriverManager.getConnection("jdbc:mysql://localhost:3306/base1",
-												"root",
-												"");
-		    System.out.println("connecté!");
+			
+			// Connection est l'interface décrivant un objet permettant de se connecter a une base de donnée
+			// le DriverManager renvoie un objet implémentant cette interface qui est capable de gérer l'url qu'on lui a fournie
+		    Connection base = DriverManager.getConnection("jdbc:mysql://localhost:3306/bddarticles","root","");
+		    System.out.println("connecté à la base de données!");
 		    
 		    //  je creer mon objet permettant de requetter la base
 		    Statement request = base.createStatement();
 		    
-		    // execution d'une requette select
-		    // elle nous renvoie un resultset qui nous permet
+		    // execution d'une requette select, elle nous renvoie un "resultset" qui nous permet
 		    // de parcourir les lignes renvoyée
-		    ResultSet rs = request.executeQuery("select * from `article`");
+		    ResultSet rs = request.executeQuery("select * from `articles`");
 		    
 		    // parcourir les lignes
 		    while (rs.next()){
 		    	// récupérer les colonnes qui nous intéressent
 		    	// sur la ligne courante
-		    	System.out.println(rs.getString("nom") +
-		    			" -> " + rs.getDouble("prix"));
+		    	System.out.println("article : " + rs.getString("libelle") + " prix => " + rs.getDouble("prix")+"€");
 		    }
 		    
 		    // attention a bien libérer les ressources
