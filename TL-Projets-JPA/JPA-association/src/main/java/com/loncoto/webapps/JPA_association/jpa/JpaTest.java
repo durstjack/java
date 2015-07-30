086d8bacc1aee69407c3e9c82a335b9d1cac005a
Thierry Lalandre <durstjack@gmail.com>
2015-07-30 14:02:41 +0200

    jpa association cle => primaire & secondaire

---
+++ TL-Projets-JPA/JPA-association/src/main/java/com/loncoto/webapps/JPA_association/jpa/JpaTest.java
@@ -0,0 +1,97 @@
+package com.loncoto.webapps.JPA_association.jpa;
+
+import java.util.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import com.loncoto.webapps.SecondJPA.beans.Article;
+
+public class JpaTest {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testHibernate");
+        Scanner input = new Scanner(System.in);
+
+        input.nextLine();
+        System.out.println("--------------------------------------");
+		test1(emf);
+
+        input.nextLine();
+		System.out.println("--------------------------------------");
+		test2(emf);
+
+        input.nextLine();
+		System.out.println("--------------------------------------");
+		
+        input.nextLine();
+		System.out.println("--------------------------------------");
+		test3(emf);
+
+
+	    emf.close();
+		System.out.println("done...");
+	}
+
+
+	public static void test1(EntityManagerFactory emf)
+	{
+		// on recupere un entityManager
+		EntityManager em = emf.createEntityManager();
+		// et une transaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		//----------------------------------------------------
+
+		
+		//----------------------------------------------------
+		tx.commit();
+		em.close();
+	}
+
+
+	public static void test2(EntityManagerFactory emf)
+	{
+		// on recupere un entityManager
+		EntityManager em = emf.createEntityManager();
+		// et une transaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		//----------------------------------------------------
+		
+
+		//----------------------------------------------------
+		tx.commit();
+		em.close();
+	}
+	
+	public static void test3(EntityManagerFactory emf)
+	{
+		// on recupere un entityManager
+		EntityManager em = emf.createEntityManager();
+		// et une transaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		//----------------------------------------------------
+		
+
+		
+		//----------------------------------------------------
+		tx.commit();
+		em.close();
+	}
+
+	
+	
+	
+	
+
+}
